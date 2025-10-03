11db5693c67e018df6337984e1a3dd83bfb1c753
krishna <krishna_a@ce.iitr.ac.in>
2025-10-03 21:13:41 +0530

    "Added explanation comment for @Primary and @Qualifier"

--- src/main/java/com/example/EcommerceSpring/services/FakeStoreCategoryService.java
+++ src/main/java/com/example/EcommerceSpring/services/FakeStoreCategoryService.java
@@ -11,6 +11,36 @@ import java.util.List;
 public class FakeStoreCategoryService implements ICategoryService {
 
     private final ICategoryGateway categoryGateway;
+
+    /*
+        Currently, there is only one implementation of ICategoryGateway only.
+        So Spring can easily auto-inject the object because at runtime, there is only implementation to look for.
+
+        But now assume, there are 2 implementations of ICategoryGateway. It will throw
+        NoUniqueBeanDefinitionException. To resolve this,
+
+        1. @Primary Annotation
+            It specifies which is the default implementation that has to be used by Spring to auto-inject.
+            Usage :-
+                It makes sense when we are trying to deprecate one implementation.
+
+        2.@Qualifier
+            It kind of uses key based unique identifier.
+
+            Example :-
+                @Component("fakeStoreCategoryGateway)
+                public class FakeStoreCategoryGateway implements ICategoryGateway {
+                ...
+
+                 public FakeStoreCategoryService(@Qualifier('fakeStoreCategoryGateway) ICategoryGateway categoryGateway) {
+                ...
+
+            Usage :-
+                Now different services can use different implementation of ICategoryGateway depending upon their
+                use case.
+
+    */
+
     public FakeStoreCategoryService(ICategoryGateway categoryGateway) {
         this.categoryGateway = categoryGateway;
     }
